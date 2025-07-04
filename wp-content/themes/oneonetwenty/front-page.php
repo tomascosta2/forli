c1a981717619fc2e2f9ddf47af64ebb392d1914e
tomas <tomascostapp@gmail.com>
2025-07-04 13:58:31 -0300

    Avances

--- wp-content/themes/oneonetwenty/front-page.php
+++ wp-content/themes/oneonetwenty/front-page.php
@@ -91,7 +91,7 @@ get_header(); ?>
 <section>
 	<div class="grid md:grid-cols-5 bg-[#4F5D29]/40">
 		<img class="bg-gray-300 md:col-span-2" src="<?php echo $nosotros_fields['imagen_nosotros']['url']; ?>" alt="<?php echo $nosotros_fields['imagen_nosotros']['alt']; ?>">
-		<div class="px-[90px] py-[140px] md:col-span-3">
+		<div class="md:px-[90px] p-8 md:py-[140px] md:col-span-3">
 			<h2 class="mb-[20px] text-[28px] uppercase"><?php echo $nosotros_fields['titulo']; ?></h2>
 			<p class="text-[18px] font-light whitespace-pre-wrap"><?php echo strip_tags($nosotros_fields['descripcion']); ?></p>
 		</div>
@@ -100,13 +100,13 @@ get_header(); ?>
 
 <section class="py-[90px] bg-[#231F20]">
 	<div class="max-w-[1260px] mx-auto px-4">
-		<div class="grid grid-cols-9 gap-[60px]">
-			<div class="col-span-4">
+		<div class="grid md:grid-cols-9 gap-[60px]">
+			<div class="md:col-span-4">
 				<h3 class="text-white text-[40px] text-center leading-[110%]"><?php echo $banner2_fields['titulo'] ?></h3>
 				<img class="w-[500px] max-w-full mx-auto" src="<?php echo $banner2_fields['bajada']['url'] ?>" alt="<?php echo $banner2_fields['bajada']['alt'] ?>">
 				<a class="block mx-auto mt-12 w-fit border border-white text-white font-bold px-10 py-4 rounded-full" href="<?php echo $banner2_fields['boton']['url'] ?>"><?php echo $banner2_fields['boton']['title'] ?></a>
 			</div>
-			<div class="banner2-list col-span-5 flex items-center text-white text-[20px] font-semibold">
+			<div class="banner2-list md:col-span-5 flex items-center text-white text-[20px] font-semibold">
 				<?php echo $banner2_fields['lista'] ?>
 			</div>
 		</div>
@@ -115,7 +115,7 @@ get_header(); ?>
 
 <section class="py-[100px]">
 	<div class="max-w-[1260px] mx-auto px-4">
-		<div class="grid grid-cols-2 gap-12">
+		<div class="grid md:grid-cols-2 md:gap-12">
 			<div class="max-w-[540px]">
 				<img class="w-[459px] max-w-full mx-auto" src="<?php echo $clientes_fields['titulo']['url'] ?>" alt="<?php echo $clientes_fields['titulo']['alt'] ?>">
 				<div class="flex gap-8 flex-wrap justify-center">
@@ -128,19 +128,19 @@ get_header(); ?>
 					<img class="object-contain w-[90px] min-h-[20px]" src="<?php echo $clientes_fields['cliente_7']['url']; ?>" alt="<?php echo $clientes_fields['cliente_7']['alt']; ?>">
 				</div>
 			</div>
-			<img src="<?php echo $clientes_fields['cliente_destacado']['url']; ?>" alt="<?php echo $clientes_fields['cliente_destacado']['alt']; ?>">
+			<img class="-order-1 md:order-none" src="<?php echo $clientes_fields['cliente_destacado']['url']; ?>" alt="<?php echo $clientes_fields['cliente_destacado']['alt']; ?>">
 		</div>
 	</div>
 </section>
 
 <section>
-	<div class="grid grid-cols-5">
-		<div class="bg-white py-[150px] px-[80px] col-span-2">
+	<div class="grid md:grid-cols-5">
+		<div class="bg-white p-8 md:py-[150px] md:px-[80px] md:col-span-2">
 			<p class="uppercase text-[18px] font-bold text-[#4F5D29]"><?php echo $contacto_fields['arriba_del_titulo'] ?></p>
 			<h2 class="text-[32px] my-4 leading-[120%]"><?php echo $contacto_fields['titulo'] ?></h2>
 			<p class="text-[20px] max-w-full w-[360px] font-medium leading-[120%]"><?php echo $contacto_fields['bajada'] ?></p>
 		</div>
-		<div class="bg-red-500 col-span-3 py-[80px] px-[100px] bg-[url('<?php echo $contacto_fields['imagen']['url']; ?>')] bg-cover">
+		<div class="bg-red-500 md:col-span-3 p-8 md:py-[80px] md:px-[100px] bg-[url('<?php echo $contacto_fields['imagen']['url']; ?>')] bg-cover">
 			<?php echo do_shortcode('[contact-form-7 id="c0a4142" title="Formulario de contacto"]'); ?>
 		</div>
 	</div>
